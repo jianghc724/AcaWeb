265a5f136e8e308023c5ffb236e812fe39c46592
Haochen Jiang <jianghc724@foxmail.com>
2018-10-09 10:51:27 +0800

    second round result

--- user/models.py
+++ user/models.py
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
     firstNight = models.BooleanField(default=False)
     secondNoon = models.BooleanField(default=False)
     secondNight = models.BooleanField(default=False)
-    userStatus = models.BooleanField(default=False)
+    userStatus = models.IntegerField(default=0)
     # 1 For Sop 2 For MS 3 For Alto 4 For CT 5 For Tenor 6 For Bar 7 For Bass 8 For VP
     # 9 For Sop/Alto 10 For Alto/MS 11 For Bar/Bass 12 For Bass/Bar 13 For Bass/VP 14 For VP/Bass
     part = models.IntegerField(default=0)
